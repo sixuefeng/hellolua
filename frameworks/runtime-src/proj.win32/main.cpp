b8bf5d9b6831f2bbb40b2c08fb60acad56615f3e
sixuefeng <sixuefeng@dianhun.cn>
2017-07-04 14:12:21 +0800

    日志

--- frameworks/runtime-src/proj.win32/main.cpp
+++ frameworks/runtime-src/proj.win32/main.cpp
@@ -12,3 +12,36 @@ int WINAPI _tWinMain(HINSTANCE hInstance,
     auto simulator = SimulatorWin::getInstance();
     return simulator->run();
 }
+
+
+//#include "main.h"  
+//#include "AppDelegate.h"  
+//#include "cocos2d.h"  
+//
+//USING_NS_CC;
+//
+//#define USE_WIN32_CONSOLE    
+//
+//int WINAPI _tWinMain(HINSTANCE hInstance,
+//	HINSTANCE hPrevInstance,
+//	LPTSTR    lpCmdLine,
+//	int       nCmdShow)
+//{
+//	UNREFERENCED_PARAMETER(hPrevInstance);
+//	UNREFERENCED_PARAMETER(lpCmdLine);
+//
+//#ifdef USE_WIN32_CONSOLE    
+//	AllocConsole();
+//	freopen("CONIN$", "r", stdin);
+//	freopen("CONOUT$", "w", stdout);
+//	freopen("CONOUT$", "w", stderr);
+//	printf("hello--------------------------hello", NULL);
+//#endif    
+//
+//	// create the application instance  
+//	AppDelegate app;
+//	return Application::getInstance()->run();
+//#ifdef USE_WIN32_CONSOLE    
+//	FreeConsole();
+//#endif    
+//}
